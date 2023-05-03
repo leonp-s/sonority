8598b1357667ac5bb7b75f871437a688a7fa9a22
Micah Strange <micasans@btinternet.com>
2023-05-03 16:38:01 +0100

    NoiseTest done

--- sonority_unity/Assets/Plugins/sonority/src/SonorityIntegration.cs
+++ sonority_unity/Assets/Plugins/sonority/src/SonorityIntegration.cs
@@ -6,8 +6,17 @@ public class SonorityIntegration : MonoBehaviour
     [DllImport("sonority_engine", EntryPoint="Init")]
     public static extern void Init();
 
+    [DllImport("sonority_engine", EntryPoint="Deinit")]
+    public static extern void Deinit();
+
     void Start()
     {
         Init();
     }
+
+    void OnDestroy()
+    {
+        Deinit();
+    }
+    
 }
