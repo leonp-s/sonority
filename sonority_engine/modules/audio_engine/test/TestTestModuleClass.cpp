0549ceb4eb92134a378194caee1d1826fb818347
LeonPS <leon.ps@focusrite.com>
2023-05-20 15:44:43 +0100

    cleanup

--- sonority_engine/modules/audio_engine/test/TestTestModuleClass.cpp
+++
@@ -1,7 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-#include "sonority_test_module/TestModuleClass.h"
-
-TEST_CASE("Test Gets Module String") {
-    TestModuleClass testModuleClass;
-    REQUIRE(testModuleClass.getPrintMe() == "TESTMODULECLASS");
-}
